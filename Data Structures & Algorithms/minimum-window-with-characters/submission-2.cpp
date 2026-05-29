a82e677135308c721c47c90fcdc8fff9ae5c1c53
Rishav Sanjan <103287216+rishavsanjan@users.noreply.github.com>
2026-05-29 14:30:34 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        
+        int hash[256] = {0};
+        for(auto x : t){
+            hash[x]++;
+        }
+
+        int l = 0, r= 0, sIndex = -1, count = 0, minLen = INT_MAX;
+
+        while(r < s.size()){
+            if(hash[s[r]] > 0){
+                count++;
+            }
+            hash[s[r]]--;
+
+            while(count == t.size()){
+                if(r - l + 1 < minLen){
+                    minLen = r - l + 1;
+                    sIndex = l;
+                }
+                hash[s[l]]++;
+                if(hash[s[l]] > 0){
+                    count--;
+                }
+                l++;
+                
+            }
+            r++;
+        }
+
+        if(sIndex == -1){
+            return "";
+        }else{
+            return s.substr(sIndex, minLen);
+        }
+
+       
+    }
+};
