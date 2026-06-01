9804e7a4b95f9bb8f9edb21756fdf0567be739ff
Rishav Sanjan <103287216+rishavsanjan@users.noreply.github.com>
2026-06-01 13:47:23 +0530

    Add: maximum-frequency-stack - submission-0

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-0.cpp
@@ -0,0 +1,30 @@
+class FreqStack {
+public:
+
+    priority_queue<pair<int, pair<int, int>>>pq;
+    unordered_map<int, int>freq;
+    int pos = 0;
+    FreqStack() {
+        
+    }
+    
+    void push(int val) {
+        pq.push(make_pair(freq[val]++, make_pair(pos++, val)));
+    }
+    
+    int pop() {
+        auto val = pq.top();
+        pq.pop();
+
+        int ans = val.second.second;
+        freq[ans]--;
+        return ans;
+    }
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(val);
+ * int param_2 = obj->pop();
+ */
