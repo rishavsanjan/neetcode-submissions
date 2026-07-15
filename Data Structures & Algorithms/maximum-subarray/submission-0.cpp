b17f5c0a4293c1f1091601be9163aa9155778e69
Rishav Sanjan <103287216+rishavsanjan@users.noreply.github.com>
2026-07-15 21:37:18 +0530

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi = INT_MIN;
+        int sum = 0;
+        for(auto x : nums){
+            sum += x;
+            maxi = max(sum, maxi);
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+
+        return maxi;
+    }
+};
